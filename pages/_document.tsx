5893a976ceb1cf69cb222c3801d3e56317bcaf35
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2022-10-05 15:07:00 +0100

    feat: seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,33 +2,27 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang='en'>
 			<Head>
 				<meta name='application-name' content='next-lenis' />
 				<meta name='apple-mobile-web-app-capable' content='yes' />
 				<meta name='apple-mobile-web-app-status-bar-style' content='default' />
 				<meta name='apple-mobile-web-app-title' content='next-lenis' />
-				<meta name='description' content='We put the Glamour in Events' />
+				<meta name='description' content='' />
 				<meta name='format-detection' content='telephone=no' />
 				<meta name='mobile-web-app-capable' content='yes' />
 				<meta name='twitter:card' content='summary' />
-				<meta name='twitter:url' content='https://next-lenis.sunbeam.studio' />
+				<meta name='twitter:url' content='' />
 				<meta name='twitter:title' content='next-lenis' />
-				<meta name='twitter:description' content='We put the Glamour in Events' />
-				<meta
-					name='twitter:image'
-					content='https://next-lenis.sunbeam.studio/svg/icon.svg'
-				/>
+				<meta name='twitter:description' content='' />
+				<meta name='twitter:image' content='/svg/icon.svg' />
 				<meta name='twitter:creator' content='@JoBenEtuk' />
 				<meta property='og:type' content='website' />
 				<meta property='og:title' content='next-lenis' />
 				<meta property='og:description' content='We put the Glamour in Events' />
 				<meta property='og:site_name' content='next-lenis' />
-				<meta property='og:url' content='https://next-lenis.sunbeam.studio' />
-				<meta
-					property='og:image'
-					content='https://next-lenis.sunbeam.studio/svg/icon.svg'
-				/>
+				<meta property='og:url' content='' />
+				<meta property='og:image' content='/svg/icon.svg' />
 
 				<link rel='manifest' href='/manifest.json' />
 				<link href='/icon.svg' rel='icon' type='image/svg' sizes='16x16' />

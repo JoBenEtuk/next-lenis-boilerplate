e71aa997f5dc237736c4793efa94816746978557
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2022-11-18 09:53:31 +0100

    fix: add async

--- layout/panel/index.tsx
+++ layout/panel/index.tsx
@@ -7,30 +7,30 @@ import S from './Panel.module.scss'
 const Panel = () => {
 	const router = useRouter()
 
-	const startAnimation = () => {
-		gsap.fromTo(
-			"[data-animation='panel']",
-			{
-				y: '100%',
-			},
-			{
-				y: 0,
+	useEffect(() => {
+		const startAnimation = async () => {
+			gsap.fromTo(
+				"[data-animation='panel']",
+				{
+					y: '100%',
+				},
+				{
+					y: 0,
+					duration: 1.5,
+					ease: 'expo.inOut',
+				}
+			)
+		}
+
+		const endAnimation = () => {
+			gsap.to("[data-animation='panel']", {
+				y: '-100%',
+				delay: 0.5,
 				duration: 1.5,
 				ease: 'expo.inOut',
-			}
-		)
-	}
+			})
+		}
 
-	const endAnimation = () => {
-		gsap.to("[data-animation='panel']", {
-			y: '-100%',
-			delay: 0.5,
-			duration: 1.5,
-			ease: 'expo.inOut',
-		})
-	}
-
-	useEffect(() => {
 		router.events.on('routeChangeStart', startAnimation)
 		router.events.on('routeChangeComplete', endAnimation)
 		router.events.on('routeChangeError', endAnimation)
